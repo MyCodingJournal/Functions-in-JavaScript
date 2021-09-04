487fb1cf6d7dfb1c98eb5aa4c5e1947cc2a34e3c
JJ Chang <75114824+Jayz-lab@users.noreply.github.com>
2021-09-04 20:19:13 +1000

    Update js.index

--- index.js
+++ index.js
@@ -69,3 +69,69 @@ function inPercentage (num1) {
     return percentage * num1;
 }
 console.log("Hey that was a", inPercentage(2),"% return!");
+
+//-------------------------------------------------------------------------------------------------
+//Basic maths function
+function add(x,y){
+  return x+y;
+}
+add(1,2);
+
+function divide(x,y){
+  return x/y;
+}
+divide(16,2);
+
+function multiply(x,y){
+  return x*y;
+}
+multiply(4,2);
+
+function subtract(x,y){
+  return x-y;
+}
+subtract(2,1);
+
+
+//-------------------------------------------------------------------------------------------------
+let number = 5;
+
+number++;
+number; //return=> 6, the number was incremented after it was evaluated
+number--;
+number;
+
+--number;//number is evaluated after the operator is executed, but recommends placing operators after the not before like the above.
+number;
+
+++number;
+number;
+
+//It's best to use the basic arithmetic operators combined with =.
+number += 3;
+number -= 2;
+number *= 10;
+number /= 5;
+
+// note: the return value is dependent upon the value above it, takes the return value above and compute to the next.
+
+
+//---------------------------------------------------------------------------------------------------
+//if we have two functions that depend on the same external variable, the order in which they are called matters.
+
+let number = 10;
+
+function add5(){
+  return number += 5;
+}
+
+function divideBy3 (){
+  return number /= 3;
+}
+divideBy3(); //=> returns 3.3333333333333335
+add5(); //=> returns 8.333333333333334
+
+add5(); //=> returns 15
+divideBy3(); //=> returns 5;
+//Because these methods are more explicit, we prefer += to ++ and -= to -- (usually).
+
